48dcb203cdb165ed4e1287abbb886d08bc214663
Nicholas Jensen-Hay <NicholasJensenHay@gmail.com>
2016-03-23 08:33:47 -0700

    add app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from 'angular2/core';
+import { Keg       } from './keg.module';
+import { KegListComponent } from './kegList.component';
+import { NewKegComponent } from './newKeg.component';
 
 @Component({
   selector: 'my-app',
+  directives: [KegListComponent, NewKegComponent],
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <new-keg></new-keg>
+    <keg-list [kegList]="kegs"></keg-list>
   `
 })
 export class AppComponent {
-
+  public kegs: Keg[];
+  constructor() {
+    this.kegs = [
+      new Keg("Coors"),
+      new Keg("Budweiser")
+    ];
+  }
 }
